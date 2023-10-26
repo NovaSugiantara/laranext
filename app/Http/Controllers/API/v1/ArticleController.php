c5a741a09697372456f5f3be79f09f9175e0ebe4
Nova Sugiantara <novasugiantara425@gmail.com>
2023-10-26 17:03:35 +0800

    [Backend] API Resource

---
+++ app/Http/Controllers/API/v1/ArticleController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\v1\ArticleCollection;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return new ArticleCollection(Article::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Article $article)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Article $article)
+    {
+        //
+    }
+}
---
+++ app/Http/Resources/v1/ArticleResource.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Resources\v1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticleResource extends JsonResource
+{
+    public static $wrap = 'articles';
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'type' => 'articles',
+            'id' => $this->id(),
+            'attributes' => [
+                'title' => $this->title(),
+                'slug' => $this->slug(),
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at,
+            ],
+            'relationships' => [
+                'author' => AuthorResource::make($this->author())
+            ],
+            'links' => [
+                'self' => route('articles.show', $this->id()),
+                'related' => route('articles.show', $this->slug()),
+            ]
+        ];
+    }
+
+    public function with($request)
+    {
+        return [
+            'status' => 'success'
+        ];
+    }
+
+    public function withResponse($request, $response)
+    {
+        $response->header('Accept', 'application/json');
+        $response->header('Version', '1.0');
+    }
+}
